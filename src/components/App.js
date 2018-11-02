1c424b361faeaae62b62b38f48e0c630396abe0a
jamal7X7 <jamal7x7@gmail.com>
2018-11-02 23:37:39 +0000

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import '../styles/App.sass'
+
+const Header = () => (
+  <header className='App-header'>
+    <h1 className='App-title'> Syllables App </h1>
+  </header>
+)
+
+const Main = (props) => (
+  <div className='game'>
+
+    
+
+  </div>
+)
+
+const Side = () => (
+  <div className='score'>
+
+      
+
+  </div>
+)
+
+
+
+class  SyllablesApp extends Component {
+  
+  state = {
+    
+  }
+
+
+
+  componentDidMount() {
+    
+  
+
+  }
+
+
+      render () {
+    return (
+      <div className='App-container'>
+
+        <Side />
+        <Main />
+       
+
+      </div>
+    )
+  }
+}
+
+const App = (props) => (
+  <div className='App'>
+    <Header />
+    < SyllablesApp />
+
+  </div>
+)
+
+export default App
+
